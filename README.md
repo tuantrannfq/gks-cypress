8294b36d5e2a0f4e05e9759eb8346927e11f5b59
Tuan Tran <tuan.tran@nfq.asia>
2022-02-22 14:50:59 +0700

    add env

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gks-cypress
+Install
+```
+yarn install
+```
+Run cypress
+```
+yarn run cypress open
+```
--- cypress/integration/GKS-mvp-ga/login.js
+++ cypress/integration/GKS-mvp-ga/login.js
@@ -1,26 +1,26 @@
 describe('Login', () => {
     beforeEach(() => {
-        cy.visit('http://mvpga.local/login.php')
+        cy.visit(Cypress.env('host')+'/login.php')
     })
 
     it('Input username', () => {
         cy.get('#loginpage .username')
-            .type('admin')
-            .should('have.value', 'admin')
+            .type(Cypress.env('username'))
+            .should('have.value', Cypress.env('username'))
     })
 
     it('Input password', () => {
         cy.get('#loginpage .password')
-            .type('12345678')
-            .should('have.value', '12345678')
+            .type(Cypress.env('password'))
+            .should('have.value', Cypress.env('password'))
     })
 
     it('Press login', () => {
         cy.get('#loginpage .username')
-            .type('admin');
+            .type(Cypress.env('username'));
 
         cy.get('#loginpage .password')
-            .type('12345678');
+            .type(Cypress.env('password'));
 
         cy.get('#loginpage .btn-login').click();
         
